3b91eea59ba44977131d991e6eef5fca1895a88b
Artistry449 <artistry449@gmail.com>
2024-06-14 17:50:57 +0800

    added Answer UI

--- app/page.tsx
+++ app/page.tsx
@@ -4,10 +4,10 @@ export default function Home() {
   return (
     <div className="h-full py-10 px-20">
       {/* <header className="p-2"> */}
-        <p className="font-mono font-bold text-4xl mb-1">Chatbot</p>
+      <p className="font-mono font-bold text-4xl mb-1">Chatbot</p>
       {/* </header> */}
       {/* <main className="h-full"> */}
-        <Container />
+      <Container />
       {/* </main> */}
     </div>
   );

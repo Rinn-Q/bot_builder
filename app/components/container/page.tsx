54e5e0d9dc7cd3ca21c0c656409119c17a3097cb
jawhlan <enhpurevjawhlan@gmail.com>
2024-06-17 17:49:39 +0800

    icon oruulsan

--- app/components/container/page.tsx
+++ app/components/container/page.tsx
@@ -113,7 +113,7 @@ export default function Container() {
                 </div>
                 <div className="box-shadow w-3/5 p-8 rounded-xl bg-white">
                     {
-                        answer ? (
+                        answer !== undefined ? (
                             <Answer width={100} caption="" description={answer.answer_content} height={100} id={0} choice_id={0} />
                         ) : (
                             <AnswerSkelton />
--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,12 @@
 import Container from "./components/container/page";
-
+import img from '../public/img/limebot-icon.png';
 export default function Home() {
   return (
     <div className="h-full py-10 px-20 bg-slate-100">
-      {/* <header className="p-2"> */}
-      <p className="font-mono font-bold text-4xl mb-1">Chatbot</p>
-      {/* </header> */}
-      {/* <main className="h-full"> */}
+      <div className="flex items-center">
+        <p className="font-mono font-bold text-4xl mb-1 mr-2"><span className=" text-lime-500 font-nunito font-extrabold">LIME</span><span className="font-mono text-4xl">Bot</span></p>
+        <img src={`img/limebot-icon.png`} alt="" />
+      </div>
       <Container />
       {/* </main> */}
     </div>

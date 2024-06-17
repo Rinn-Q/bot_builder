2659cbee8bd4b88acc769f186808ceac233973fb
Huranu <hulan0000@gmail.com>
2024-06-17 11:41:50 +0800

    added custom button data

--- app/components/popper/page.tsx
+++ app/components/popper/page.tsx
@@ -1,13 +1,20 @@
 import * as React from 'react';
-import { Popper, Paper, Grow, ClickAwayListener, MenuList, MenuItem } from '@mui/material';
+import { Popper, Paper, Grow, ClickAwayListener, MenuList, MenuItem, SvgIconTypeMap } from '@mui/material';
+import { OverridableComponent } from '@mui/material/OverridableComponent';
 
 interface CustomPopperProps {
     open: boolean;
     anchorRef: React.RefObject<HTMLButtonElement>;
     handleClose: (event: Event | React.SyntheticEvent) => void;
+    buttonData: Button[]
 }
 
-const CustomPopper: React.FC<CustomPopperProps> = ({ open, anchorRef, handleClose }) => {
+interface Button {
+    title : String;
+    icon : OverridableComponent<SvgIconTypeMap<{}, "svg">>
+}
+
+const CustomPopper: React.FC<CustomPopperProps> = ({ open, anchorRef, handleClose , buttonData}) => {
     return (
         <Popper
             open={open}
@@ -26,9 +33,12 @@ const CustomPopper: React.FC<CustomPopperProps> = ({ open, anchorRef, handleClos
                     <Paper>
                         <ClickAwayListener onClickAway={handleClose}>
                             <MenuList autoFocusItem={open} id="composition-menu" aria-labelledby="composition-button">
-                                <MenuItem onClick={handleClose}>Profile</MenuItem>
-                                <MenuItem onClick={handleClose}>My account</MenuItem>
-                                <MenuItem onClick={handleClose}>Logout</MenuItem>
+                                {buttonData.map((e)=>
+                                <MenuItem onClick={handleClose}>
+                                    <e.icon></e.icon>
+                                    {e.title}
+                                    </MenuItem>
+                                )}
                             </MenuList>
                         </ClickAwayListener>
                     </Paper>
